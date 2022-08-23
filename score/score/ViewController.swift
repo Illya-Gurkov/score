535eba9d3f8e7b2a14265898853cdd72949b2ad5
Illya-Gurkov <109282408+Illya-Gurkov@users.noreply.github.com>
2022-08-23 12:45:16 +0300

    new project

---
+++ score/score/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  score
+//
+//  Created by Illya Gurkov on 23.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
